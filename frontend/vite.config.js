82a4ced90c9fe6b45261a9d807cd376f7bfbafc3
yujeonglee623 <yujeonglee623@gmail.com>
2026-03-05 14:16:45 +0900

    포트 수정

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     open: true,
     proxy: {
       '/api': {
-        target: 'http://127.0.0.1:8001',
+        target: 'http://127.0.0.1:8000',
         changeOrigin: true,
       }
     }
